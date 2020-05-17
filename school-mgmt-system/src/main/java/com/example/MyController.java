d33f5bae7a238e44ad1eb9c8c8c8d98e0a415a53
Rupendra Sharma <rupendra1817@gmail.com>
2020-05-17 20:33:11 +0530

    Added second line.

--- school-mgmt-system/src/main/java/com/example/MyController.java
+++ school-mgmt-system/src/main/java/com/example/MyController.java
@@ -9,6 +9,7 @@ public class MyController {
 	@GetMapping(value = "/welcome-message")
 	public String getWelcomeMsg() {
 		System.out.println("In sidegetWelcomeMsg() method.");
+		System.out.println("Added second line.");
 		return "Hello";
 	}
 }
